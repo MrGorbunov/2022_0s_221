1151e28018df1e3234d72e401df33365d1b4b337
MrGorbunov <volarcko@gmail.com>
2022-02-12 23:21:30 -0500

    Completed bstLevelOrder

--- pa2/bstLevelOrder/bstLevelOrder.c
+++ pa2/bstLevelOrder/bstLevelOrder.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 // A program to perform a LEVEL ORDER (BREADTH-FIRST) TRAVERSAL of a binary search tree
 
@@ -25,8 +26,11 @@ void add_to_bst (struct BSTNode* root, int value) {
 		child_to_travel_to = &(root->r_child);
 	}
 
-	if (*child_to_travel_to == NULL) {
-		*child_to_travel_to = malloc(sizeof(struct BSTNODE));	
+	if (!*child_to_travel_to) {
+		*child_to_travel_to = calloc(1, sizeof(BSTNode));
+		(*child_to_travel_to)->key = value;
+	} else {
+		add_to_bst(*child_to_travel_to, value);
 	}
 }
 
@@ -44,36 +48,143 @@ void free_bst (struct BSTNode* root) {
 // LINKED LIST IMPLEMENTATION OF QUEUE
 
 // queue needed for level order traversal
-/* ... */
+typedef struct Queue Queue;
+struct Queue {
+	Queue* nextNode;
+	BSTNode* bstNode;
+};
 
 // Append a new QueueNode to the back of the Queue
 void enqueue ( Queue* queue, BSTNode* data ) {
-  /* ... */
+	// First traverse to the bottom of the tree
+	Queue* prevQueue = queue;
+	while (queue != NULL) {
+		prevQueue = queue;
+		queue = queue->nextNode;
+	}
+
+	Queue* nextQueueNode = calloc(1, sizeof(Queue));
+	nextQueueNode->bstNode = data;
+	prevQueue->nextNode = nextQueueNode;
 }
+
+// Frees the front node of the queue after
+// pointing the passsed BSTNode** to the node's data
+void dequeue (Queue** queue, BSTNode** data) {
+	*data = (*queue)->bstNode;
+	Queue* queueToFree = *queue;
+	*queue = (*queue)->nextNode;
+	free(queueToFree);
+}
+
+void testQueue();
+void testBST();
 
 
 int main ( int argc, char* argv[] ) {
 
-    // READ INPUT FILE TO CREATE BINARY SEARCH TREE
+	//
+	// Step 1 : Construct tree
+	//
+
     FILE* fp = fopen(argv[1], "r");
     if (!fp) {
         perror("fopen failed");
         return EXIT_FAILURE;
     }
-    BSTNode* root = NULL;
+
+	// calloc is used to clear the data
+	// malloc will break this
+	BSTNode* root = calloc(1, sizeof(BSTNode)); 
+	fscanf(fp, "%d", &(root->key));
     int key;
     while ( fscanf(fp, "%d", &key)!=EOF ) {
-        root = insert (root, key);
+		add_to_bst(root, key);
     }
     fclose(fp);
 
-    // USE A QUEUE TO PERFORM LEVEL ORDER TRAVERSAL
-    Queue queue = { .front=NULL, .back=NULL };
-    BSTNode* currBSTNode = root;
-    do {
-        /* ... */
-    } while ( currBSTNode!=NULL );
 
-    delete_bst(root);
+	//
+	// Step 2 : Populate Queue
+	//
+
+	Queue* levelOrderQueue = calloc(1, sizeof(Queue));
+	levelOrderQueue->bstNode = root;
+
+	while (levelOrderQueue != NULL) {
+		BSTNode* nextVal = levelOrderQueue->bstNode;
+		if (nextVal != NULL) {
+			enqueue(levelOrderQueue, nextVal->l_child);
+			enqueue(levelOrderQueue, nextVal->r_child);
+			printf("%d ", nextVal->key);
+		}
+
+		Queue* prevQueueNode = levelOrderQueue;
+		levelOrderQueue = levelOrderQueue->nextNode;
+		free(prevQueueNode);
+	}
+
+	printf("\n");
+
+    free_bst(root);
     return EXIT_SUCCESS;
 }
+
+
+
+
+
+
+void testQueue () {
+	printf(" == Testing Queue ==\n\n");
+
+	BSTNode* root = calloc(1, sizeof(BSTNode));
+	root->key = 10;
+	add_to_bst(root, 9);
+	add_to_bst(root, 8);
+	add_to_bst(root, 7);
+	add_to_bst(root, 6);
+
+	Queue* myQ = calloc(1, sizeof(Queue));
+	myQ->bstNode = root;
+
+	enqueue(myQ, root->l_child);
+	enqueue(myQ, root->l_child->l_child);
+	enqueue(myQ, root->l_child);
+	enqueue(myQ, root->l_child->l_child);
+	enqueue(myQ, root->l_child);
+	enqueue(myQ, root->l_child->l_child);
+
+	while (myQ != NULL) {
+		BSTNode* data;
+		dequeue(&myQ, &data);
+		printf("Pulled data: %d\n", data->key);
+	}
+
+	free_bst(root);
+}
+
+
+void testBST () {
+	BSTNode* root = NULL;
+	root = calloc(1, sizeof(BSTNode));
+	root->key = 10;
+
+	printf("\n\nAdding 9\n");
+	add_to_bst(root, 9);
+
+	printf("\n\nAdding 8\n");
+	add_to_bst(root, 8);
+
+	printf("\n\nAdding 8 again\n");
+	add_to_bst(root, 8);
+
+	printf("Root Val: %d\n", root->key);
+	printf("root.l  : %d\n", root->l_child->key);
+	printf("root.l.l: %d\n", root->l_child->l_child->key);
+
+	free_bst(root);
+}
+
+
+
