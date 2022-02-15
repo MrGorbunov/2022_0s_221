9d716a2291dcc7ff6db8b1cabf39080eb31870de
MrGorbunov <volarcko@gmail.com>
2022-02-15 15:04:58 -0500

    Functional bash script

--- bash_test.sh
+++ bash_test.sh
@@ -16,17 +16,13 @@
 startDir=$(pwd)
 paDirs=$(ls -al | grep -o "pa[0-9]\+\$")
 
-echo $(pip freeze)
-
-# for d in $paDirs
-for d in pa0
+for d in $paDirs
 do
 	echo ""
 
 	# Go into a pa (performance assesment) directory
 	cd $startDir
 	cd $d
-	echo $d
 
 	gradingFile=$(ls -al | grep -o "assignment_autograder.py")
 	if ! [[ $gradingFile =~ ^assignment_autograder.py$ ]]; then
@@ -34,15 +30,56 @@ do
 		continue
 	fi
 
-	echo "It's here"
-	testOutput=$(python assignment_autograder.py)
-	finalScore=$(grep "\d out of" <<< "$testOutput")
+	# Run autograder and extract scores & program names
+	testOut=$(python assignment_autograder.py)
+	outScores=$(grep -o "[0-9]\+ out of [0-9]\+" <<< $testOut)
+	outPrograms=$(grep -o "Score on [^:]\+:" <<< $testOut)
 
-	echo "\/ \/"
-	echo
-	echo $finalScore
-	echo 
-	echo $testOutput
+	arrScores=($(grep -o "[0-9]\+" <<< $outScores))
+	arrPrograms=($(grep -o "[^ ]\+:" <<< $outPrograms | grep -o "[^:]\+"))
+
+	# Process scores
+	#
+	# If the assigment score is max, we don't need to do anything.
+	# Otherwise we need to print out an itemized breakdown of which
+	# tests failed
+
+	maxInd=${#arrPrograms[@]}
+	let maxInd--
+
+	let maxScoreInd=maxInd*2
+	scoreTotal=${arrScores[maxScoreInd]}
+	let maxScoreInd++
+	scoreMax=${arrScores[maxScoreInd]}
+
+
+	if [[ $scoreTotal == $scoreMax ]]; then
+		# Passed entire assigment
+		echo "[x] $d"
+	elif [[ $scoreTotal == 0 ]]; then
+		echo "[ ] $d"
+	else
+		echo "[~] $d"
+	fi
+
+	# Now print out the itemized report
+	for (( i=0; i<${#arrPrograms[@]}; i++))
+	do
+		programName=${arrPrograms[i]}
+
+		let scoreInd=$i*2
+		scoreGot=${arrScores[scoreInd]}
+		let scoreInd++
+		scoreMax=${arrScores[scoreInd]}
+
+		if [[ $scoreGot == $scoreMax ]]; then
+			echo "  - [x] $programName: $scoreGot/$scoreMax"
+		elif [[ $scoreGot == 0 ]]; then
+			echo "  - [ ] $programName: $scoreGot/$scoreMax"
+		else
+			echo "  - [~] $programName: $scoreGot/$scoreMax"
+		fi
+	done
 done
 
 
